5e39b714935dade07b62cafcd627dc350af19841
Crucifer-69 <70117507+Crucifer-69@users.noreply.github.com>
2024-06-11 19:23:11 -0400

    lab-assessment

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+const studentName = "Mohit";
+const studentNumber = 1156056;
+const result = studentName.concat('-',studentNumber);
+console.log(result);
+
+let headerContent = document.querySelector("h1");
+headerContent.innerHTML = "Mohit - 1156056"; 
+headerContent.classlist.add(heading1);
